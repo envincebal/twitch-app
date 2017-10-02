42f2eea335a4a1097748edea5b1a760a689ac3ca
Vincent Yan <vincentyan1@gmail.com>
2017-10-01 17:36:06 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,50 @@
+function getPlayers(){
+  var players = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+	for (let i = 0; i < players.length; i++) {
+		var endpoint = "https://wind-bow.glitch.me/twitch-api/streams/" + players[i];
+		var display = $(".display");
+
+		$.ajax({
+			url: endpoint,
+			type: "GET",
+			dataType: "jsonp",
+			success : function(data){
+				
+				if (data.stream !== null) {
+					var $logo = "<div class='list-item online'><div class='profile-img'><img src='"+ data.stream.channel.logo + "' class='avatar'></div>";
+					var $display_name = "<div class='screen-name'><a href='" + data.stream.channel.url + "' class='link' target='_blank'>" + data.stream.channel.display_name + "</a></div>";;
+					var $status = "<div class='status'><span class='current'>" + data.stream.game + " : " + data.stream.channel.status + "</span></div></div>";
+
+					display.append($logo + $display_name + $status);
+
+				} else {
+					var endpoint2 = "https://wind-bow.glitch.me/twitch-api/channels/" + players[i];
+
+					$.ajax({
+						url: endpoint2,
+						type: "GET",
+						dataType: "jsonp",
+						success: function(data2){
+              console.log(data);
+								var html = "";
+								console.log(data2.display_name + " is offline");
+								html += "<div class='list-item offline'><div class='profile-img'><img src='"+ data2.logo + "' class='avatar'></div>";
+								html += "<div class='screen-name'><a href='" + data2.url + "' class='link' target='_blank'>" + data2.display_name + "</a></div>";
+								html += "<div class='status'><span class='current'>Offline</span></div></div>";
+
+								display.append(html);
+						}	
+					});
+				}
+			}
+		})
+	}
+}
+
+$(document).ready(function(){
+	getPlayers();
+	
+  
+});
+
